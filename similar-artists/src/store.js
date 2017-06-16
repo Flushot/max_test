d14d2de19b696f9a6d625a262bcd5ee84d41e14b
Chris Lyon <flushot@gmail.com>
2017-06-15 23:37:29 -0700

    removed unused import

--- similar-artists/src/store.js
+++ similar-artists/src/store.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import reducer from './reducers';
-import * as Actions from './actions';
 
 const middleware = [
     thunkMiddleware,
